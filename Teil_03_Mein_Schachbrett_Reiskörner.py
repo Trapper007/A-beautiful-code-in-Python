f69ca8dc7ad22144a6776b7a12584190fb1d658e
Trapper007 <github.mf@mdcv-web.de>
2019-06-09 14:54:09 +0200

    Mein Teil03 funktioniert jetzt.

--- Teil_03_Mein_Schachbrett_Reiskörner.py
+++ Teil_03_Mein_Schachbrett_Reiskörner.py
@@ -1,10 +1,11 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
 summe = 0
-feldliste = [] 
+anzahlliste = []
 
 for feld in range(64):
     reiskörner = 2**feld
-    feldliste.append(reiskörner)
+    anzahlliste.append(reiskörner)
     summe += reiskörner
     print(f"Feld {feld+1}. = {reiskörner:>27,} Reiskörner und damit insgesamt"
           f"{summe:>28,} Reiskörner")
@@ -15,5 +16,5 @@ print()
 print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten"
       f" Reiskörner {gewicht:,.0f} Tonnen")
 
-plt.plot(feldliste)
-plt.show
+plt.plot(anzahlliste)
+plt.show()
